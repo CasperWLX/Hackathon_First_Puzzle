cae65413a0fe4e12542bd0287d2e95b7dfd7f0f1
Hampus Källberg <hampuskallberg@gmail.com>
2024-11-07 15:10:57 +0100

    Happy Coding!

---
+++ src/Decipher.java
@@ -0,0 +1,24 @@
+/**
+ * The challenge is to decrypt the encoded message.
+ * If you haven't figured out what kind of cipher this is yet, take on more look on the website.
+ * You might be able to find a clue.
+ *
+ * The rest is up to everyone's imagination. Automate it or do it manually.
+ * GET CREATIVE!
+ *
+ * Try not to use chatGPT, you'll have more fun without it.
+ */
+
+public class Decipher implements IDecipher {
+
+    //You can assign the encrypted message to this variable
+    private String encryptedText;
+
+    /**
+     * Place logic inside this method
+     * @param encryptedText - the secret message
+     */
+    public void decipherText(String encryptedText) {
+
+    }
+}
---
+++ src/IDecipher.java
@@ -0,0 +1,3 @@
+public interface IDecipher {
+    void decipherText(String encryptedText);
+}
